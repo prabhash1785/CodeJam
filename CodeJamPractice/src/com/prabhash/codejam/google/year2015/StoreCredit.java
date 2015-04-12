61a8e98f0538b0631e96df8a079716de164574c4
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-12 00:44:36 -0700

    Repository to solve code jam problems

---
+++ CodeJamPractice/src/com/prabhash/codejam/google/year2015/StoreCredit.java
@@ -0,0 +1,20 @@
+package com.prabhash.codejam.google.year2015;
+
+/**
+ * Problem:
+ * You receive a credit C at a local store and would like to buy two items. You first walk through the store and create a list L of 
+ * all available items. From this list you would like to buy two items that add up to the entire value of the credit. The solution you 
+ * provide will consist of the two integers indicating the positions of the items in your list (smaller number first).
+ * 
+ * @author prrathore
+ *
+ */
+
+public class StoreCredit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
