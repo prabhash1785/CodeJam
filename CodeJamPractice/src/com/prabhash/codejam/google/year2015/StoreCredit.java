717a9e95fec5328a92245e20b35fb0d6c6117293
Prabhash Rathore <prabhashrathore@gmail.com>
2015-04-13 18:28:00 -0700

    Brute Force quadratic time algorithm to solve credit problem from google code jam

--- CodeJamPractice/src/com/prabhash/codejam/google/year2015/StoreCredit.java
+++ CodeJamPractice/src/com/prabhash/codejam/google/year2015/StoreCredit.java
@@ -12,9 +12,61 @@ package com.prabhash.codejam.google.year2015;
 
 public class StoreCredit {
 	
+	/**
+	 * Brute Force Algorithm to find the position of exact two items in the given list to be bought whose sum is equal to
+	 * the given credit.
+	 * 
+	 * Time Complexity: O(n ^ 2)
+	 * Space Complexity: O(1)
+	 * 
+	 * @param credit
+	 * @param numberOfItems
+	 * @param itemPrice
+	 */
+	public static void buyTwoItems(int credit, int numberOfItems, final int[] itemPrice) {
+		
+		int firstItemPos = -1;
+		int secondItemPos = -1;
+		
+		for(int i = 0; i < numberOfItems - 1; i++) {
+			firstItemPos = i;
+			
+			for(int j = i + 1; j < numberOfItems; j++) {
+				
+				if(itemPrice[firstItemPos] + itemPrice[j] == credit) {
+					secondItemPos = j;
+					break;
+				}
+				
+			}
+			
+			if(secondItemPos > 0) {
+				break;
+			}
+		}
+		
+		System.out.println("First item position: " + (firstItemPos + 1));
+		System.out.println("Seocnd item position: " + (secondItemPos + 1));
+		
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		
-
+//		int credit = 100;
+//		int numberOfItems = 3;
+//		int[] itemList = {5, 75, 25};
+		
+//		int credit = 200;
+//		int numberOfItems = 7;
+//		int[] itemList = {150, 24, 79, 50, 88, 345, 3};
+		
+		int credit = 8;
+		int numberOfItems = 8;
+		int[] itemList = {2, 1, 9, 4, 4, 56, 90, 3};
+		
+		buyTwoItems(credit, numberOfItems, itemList);
+		
 	}
 
 }
